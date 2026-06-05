5b79a05827a73ade60b1fb38557c720b72e6d421
Gleason <GleasonsPhotography@gmail.com>
2026-06-04 23:35:03 -0700

    changed the access code box

--- app/private-access/page.tsx
+++ app/private-access/page.tsx
@@ -33,7 +33,7 @@ const handleAccess = () => {
         placeholder="Enter Access Code"
         value={code}
         onChange={(e) => setCode(e.target.value)}
-        className="text-black px-4 py-2 rounded"
+        className="w-80 px-4 py-3 rounded-lg bg-white text-black border border-gray-400 mb-4"
       />
 
       <button
